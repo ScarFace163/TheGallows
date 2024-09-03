8398fe4fe6285f9a57b3fd07a7db9d2c3f5fe591
ScarFace163 <rarman321123@gmail.com>
2024-09-03 13:41:52 +0300

    Добавлен сервис для работы с сессией

---
+++ src/main/java/backend/academy/service/SessionService.java
@@ -0,0 +1,9 @@
+package backend.academy.service;
+
+import backend.academy.model.Session;
+
+public interface SessionService {
+    boolean isGameEnded(Session session);
+    boolean checkLetter(Session session, String letter);
+    void putLetter(Session session, String letter);
+}
---
+++ src/main/java/backend/academy/service/SessionServiceImpl.java
@@ -0,0 +1,36 @@
+package backend.academy.service;
+
+import backend.academy.model.Session;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SessionServiceImpl implements SessionService {
+
+    @Override
+    public boolean isGameEnded(Session session) {
+
+        return String.valueOf(session.currentGuess()).equals(String.valueOf(session.answer().VALUE())) || session.currentAttemptsNumber() >= session.maxAttemptsNumber();
+    }
+    @Override
+    public boolean checkLetter(Session session, String letter) {
+        if( !letter.isEmpty() && !findLetterPositionInAnswer(session, letter).isEmpty()) return true;
+        else session.currentAttemptsNumber(session.currentAttemptsNumber()+1);
+        return false;
+    }
+
+    @Override
+    public void putLetter(Session session, String letter) {
+        for (int i :findLetterPositionInAnswer(session,letter)){
+            session.currentGuess()[i] = Character.toLowerCase(letter.charAt(0));
+        }
+    }
+    private List<Integer> findLetterPositionInAnswer(Session session, String letter){
+        char[] answerValue = session.answer().VALUE();
+        List<Integer> positions = new ArrayList<>();
+        for (int i =0 ; i<answerValue.length; i++){
+            if (answerValue[i] == Character.toLowerCase(letter.charAt(0))) positions.add(i);
+        }
+        return positions;
+    }
+
+}
