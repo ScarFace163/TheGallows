b50c420a6fba3daf7cdd5548aca6a99337ece00d
ScarFace163 <rarman321123@gmail.com>
2024-09-03 13:44:37 +0300

    Изменен Main , добавлен вызов метода control класса MainController.

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,14 @@
 package backend.academy;
 
+import backend.academy.controller.MainController;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
+    MainController mainController;
     public static void main(String[] args) {
+        mainController = new MainController();
+        mainController.control();
     }
 }
+
