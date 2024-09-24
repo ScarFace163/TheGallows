181135391660969b4432c3f10a1edff11eb7506f
ScarFace163 <rarman321123@gmail.com>
2024-09-24 11:08:34 +0300

    Исправлены codeStyle

--- src/main/java/backend/academy/service/CategoryServiceImpl.java
+++ src/main/java/backend/academy/service/CategoryServiceImpl.java
@@ -52,26 +52,40 @@ public class CategoryServiceImpl implements CategoryService {
 
     private void setInitialData() {
         List<Word> animalsList = new ArrayList<>();
-        animalsList.add(new Word(new char[] {'c', 'o', 'w'}, "Makes milk", Difficult.EASY));
-        animalsList.add(new Word(new char[] {'c', 'a', 't'}, "Drinks milk", Difficult.EASY));
-        animalsList.add(new Word(new char[] {'d', 'o', 'g'}, "Best friend of human", Difficult.EASY));
-        animalsList.add(new Word(new char[] {'e', 'l', 'e', 'p', 'h', 'a', 'n', 't'}, "Have a big nose", Difficult.MEDIUM));
-        animalsList.add(new Word(new char[] {'d', 'u', 'c', 'k', 'b', 'i', 'l', 'l'}, "Half a duck", Difficult.HARD));
+        animalsList.add(new Word(new char[] {'c', 'o', 'w'},
+            "Makes milk", Difficult.EASY));
+        animalsList.add(new Word(new char[] {'c', 'a', 't'},
+            "Drinks milk", Difficult.EASY));
+        animalsList.add(new Word(new char[] {'d', 'o', 'g'},
+            "Best friend of human", Difficult.EASY));
+        animalsList.add(new Word(new char[] {'e', 'l', 'e', 'p', 'h', 'a', 'n', 't'},
+            "Have a big nose", Difficult.MEDIUM));
+        animalsList.add(new Word(new char[] {'d', 'u', 'c', 'k', 'b', 'i', 'l', 'l'},
+            "Half a duck", Difficult.HARD));
         categoryMap.put("Animal", animalsList);
         List<Word> professions = new ArrayList<>();
-        professions.add(new Word(new char[] {'t', 'e', 'a', 'c', 'h', 'e', 'r'}, "That make dumb people smart", Difficult.MEDIUM));
-        professions.add(new Word(new char[] {'p', 'o', 'l', 'i', 'c', 'e', 'm', 'a', 'n'}, "Catches bad guys", Difficult.MEDIUM));
-        professions.add(new Word(new char[] {'c', 'l', 'o', 'w', 'n'}, "Scary one", Difficult.EASY));
-        professions.add(new Word(new char[] {'p', 'o', 's', 't', 'm', 'a', 'n'}, "Backenders use this", Difficult.HARD));
+        professions.add(new Word(new char[] {'t', 'e', 'a', 'c', 'h', 'e', 'r'},
+            "That make dumb people smart", Difficult.MEDIUM));
+        professions.add(new Word(new char[] {'p', 'o', 'l', 'i', 'c', 'e', 'm', 'a', 'n'},
+            "Catches bad guys", Difficult.MEDIUM));
+        professions.add(new Word(new char[] {'c', 'l', 'o', 'w', 'n'},
+            "Scary one", Difficult.EASY));
+        professions.add(new Word(new char[] {'p', 'o', 's', 't', 'm', 'a', 'n'},
+            "Backenders use this", Difficult.HARD));
         categoryMap.put("Profession", professions);
         List<Word> backendWords = new ArrayList<>();
-        backendWords.add(new Word(new char[] {'a', 'p', 'i'}, "Interface", Difficult.EASY));
-        backendWords.add(new Word(new char[] {'d', 'a', 't', 'a', 'b', 'a', 's', 'e'}, "Helps keep data in it", Difficult.MEDIUM));
-        backendWords.add(new Word(new char[] {'s', 'p', 'r', 'i', 'n', 'g'}, "Java framework", Difficult.MEDIUM));
+        backendWords.add(new Word(new char[] {'a', 'p', 'i'},
+            "Interface", Difficult.EASY));
+        backendWords.add(new Word(new char[] {'d', 'a', 't', 'a', 'b', 'a', 's', 'e'},
+            "Helps keep data in it", Difficult.MEDIUM));
+        backendWords.add(new Word(new char[] {'s', 'p', 'r', 'i', 'n', 'g'},
+            "Java framework", Difficult.MEDIUM));
         backendWords.add(
-            new Word(new char[] {'s', 'e', 'c', 'u', 'r', 'i', 't', 'y'}, "It will help you not to be deceived", Difficult.MEDIUM));
+            new Word(new char[] {'s', 'e', 'c', 'u', 'r', 'i', 't', 'y'},
+                "It will help you not to be deceived", Difficult.MEDIUM));
         backendWords.add(
-            new Word(new char[] {'m', 'i', 'c', 'r', 'o', 's', 'e', 'r', 'v', 'i', 'c', 'e', 's'}, "Divided into parts",
+            new Word(new char[] {'m', 'i', 'c', 'r', 'o', 's', 'e', 'r', 'v', 'i', 'c', 'e', 's'},
+                "Divided into parts",
                 Difficult.HARD));
         categoryMap.put("Backend", backendWords);
 
